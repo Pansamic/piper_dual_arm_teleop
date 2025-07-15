706609f7066dbca2c35ef1e2048bce4d71f8307f
Pansamic <pansamic@foxmail.com>
2025-07-15 14:48:46 +0800

    chore:Change default CAN bus interface name.

--- script/can_bringup.sh
+++ script/can_bringup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo ip link set can0 type can bitrate 1000000
 sudo ip link set can1 type can bitrate 1000000
-sudo ip link set can0 up
+sudo ip link set can2 type can bitrate 1000000
 sudo ip link set can1 up
+sudo ip link set can2 up
