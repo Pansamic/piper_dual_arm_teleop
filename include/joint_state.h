e720ee9ec8b0a68c0387cd01a16358e5fef7980f
Pansamic <pansamic@foxmail.com>
2025-08-08 18:59:37 +0800

    feat: Rename file and update joint state structure.

--- include/joint_state.h
+++ include/joint_state.h
@@ -1,29 +1,29 @@
 /**
- * @file waypoint.h
+ * @file joint_state.h
  * @author pansamic (pansamic@foxmail.com)
- * @brief Arm trajectory waypoint definition.
+ * @brief Joint state structure for robot arm control.
  * @version 0.1
  * @date 2025-06-30
  * 
  * @copyright Copyright (c) 2025
  * 
  */
-#ifndef __WAYPOINT_H__
-#define __WAYPOINT_H__
+#ifndef __JOINT_STATE_H__
+#define __JOINT_STATE_H__
 
 #include <Eigen/Core>
-#include <piper_model.hpp>
 
+template <typename T, std::size_t NumDof>
 struct JointState
 {
     /* Joint Position */
-    Eigen::Vector<double, PiperArmNumDof> joint_pos;
+    Eigen::Vector<T, NumDof> joint_pos;
     /* Joint Velocity */
-    Eigen::Vector<double, PiperArmNumDof> joint_vel;
+    Eigen::Vector<T, NumDof> joint_vel;
     /* Joint Acceleration */
-    Eigen::Vector<double, PiperArmNumDof> joint_acc;
+    Eigen::Vector<T, NumDof> joint_acc;
     /* Joint Torque */
-    Eigen::Vector<double, PiperArmNumDof> joint_torq;
+    Eigen::Vector<T, NumDof> joint_torq;
 };
 
 #endif // __WAYPOINT_H__
