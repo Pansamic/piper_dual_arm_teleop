11c09b00bd78e4625cd9369e80f0cc7d9f950e49
Pansamic <pansamic@foxmail.com>
2025-08-10 19:53:05 +0800

    refactor: Add null client file as a placeholder,

---
+++ src/client.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file client.cpp
+ * @author pansamic (pansamic@foxmail.com)
+ * @brief Real AgileX Piper robotic manipualtor control client.
+ * @version 0.1
+ * @date 2025-08-10
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+int main(void)
+{
+
+    return 0;
+}
