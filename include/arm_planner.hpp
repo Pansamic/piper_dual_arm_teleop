1bab862cc38e8bdd84ceca8d6452a72d77810fff
Pansamic <pansamic@foxmail.com>
2025-08-12 14:38:50 +0800

    feat: Implement polynomial time duration decay algorithm into linear trajetory planner.

--- include/arm_planner.hpp
+++ include/arm_planner.hpp
@@ -38,18 +38,26 @@ public:
 
     template<typename T, std::size_t NumDof, std::size_t WayPointAmount>
     static std::tuple<std::array<TimePoint, WayPointAmount>, Eigen::Matrix<T, NumDof, WayPointAmount>>
-    plan(const TimePoint& start_time, const double min_duration, const Eigen::Vector<T, NumDof>& start, const Eigen::Vector<T, NumDof>& end)
+    plan(const TimePoint& start_time, const T min_duration, const Eigen::Vector<T, NumDof>& start, const Eigen::Vector<T, NumDof>& end)
     {
         Eigen::Matrix<T, NumDof, WayPointAmount> trajectory;
         std::array<TimePoint, WayPointAmount> time_point;
-        double duration = min_duration + CONFIG_TRAJECTORY_DECAY_COEF * (end - start).squaredNorm();
-        double interval = duration / (WayPointAmount - 1);
-        Duration step_duration = std::chrono::duration_cast<std::chrono::steady_clock::duration>(std::chrono::duration<double>(interval));
+        /* expand trajectory duration with joint position L2 norm.
+         * Avoid abrupt arm movement if a quick huge target joint position is coming. */
+        T duration = min_duration + CONFIG_TRAJECTORY_DAMPING_COEF * (end - start).squaredNorm();
+        /* Assume "n" is the waypoint amount, then there are "n-1" portions in this trajectory.
+         * The time duration of each segment is supposed to be [n-1, n-2, n-3, ... , 1].
+         * So the sum of them is n*(n-1)/2. Let's say the weight is n*(n-1)/2. */
+        static constexpr T weight = static_cast<T>(WayPointAmount * (WayPointAmount - 1) / 2);
+        T portion = duration / weight;
+        Duration time_portion = std::chrono::duration_cast<Duration>(std::chrono::duration<T>(portion));
         Eigen::Vector<T, NumDof> step_position = (end - start) / static_cast<T>(WayPointAmount - 1);
-        for (std::size_t i = 0; i < WayPointAmount; ++i)
+        TimePoint current_time = start_time;
+        for (std::size_t i = 0 ; i < WayPointAmount; ++i)
         {
             trajectory.col(i) = (start + step_position * i);
-            time_point[i] = (start_time + step_duration * i);
+            time_point[i] = current_time;
+            current_time += time_portion * ( WayPointAmount - 1 - i );
         }
         return std::make_tuple(time_point, trajectory);
     }
